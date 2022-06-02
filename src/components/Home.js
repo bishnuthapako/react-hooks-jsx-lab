d1bf2e37438ea07931da1a29bc4073688e59621e
Bishnu <bishnuthapako@gmail.com>
2022-06-01 20:34:32 -0400

    Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+ const colorStyle = {
+  color: 'firebrick'
+ };
+//  const textContent = `${name} is a Web Developer from Your ${city}`;
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={colorStyle}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
